2a19110caf565b602a3a0dae28e2ed8d7866d0c5
Claymazing <38538839+Claymazing@users.noreply.github.com>
2019-07-12 14:39:52 -0400

    Added an init function

--- craigsGUI.py
+++ craigsGUI.py
@@ -1,6 +1,23 @@
-#Just testing out the GIT and creating a file
 from tkinter import *
+from tkinter import ttk
+def __init__(self, link_list, image_list, price_list, city_list):
+    self.linkList = link_list
+    self.imageList = image_list
+    self.priceList = price_list
+    self.cityList = city_list
 root = Tk()
-w = Label(root,text = "Hello world")
-w.pack()
+
+frame = Frame(root)
+
+labelText = StringVar()
+
+label = Label(frame, textvariable = labelText)
+button = Button(frame, text = "Click me")
+
+labelText.set("I am a label")
+
+label.pack()
+button.pack()
+frame.pack()
+
 root.mainloop()
