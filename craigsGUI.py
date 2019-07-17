2a5275c927957c267d48549392c4b7e177f71dc6
Claymazing <38538839+Claymazing@users.noreply.github.com>
2019-07-17 11:26:36 -0400

    made a canvas widget which can display the images in the 'TestImages' directory. Added the 'TestImages' directory

--- craigsGUI.py
+++ craigsGUI.py
@@ -1,23 +1,32 @@
 from tkinter import *
-from tkinter import ttk
-def __init__(self, link_list, image_list, price_list, city_list):
-    self.linkList = link_list
-    self.imageList = image_list
-    self.priceList = price_list
-    self.cityList = city_list
+from PIL import ImageTk, Image
+from io import BytesIO
+import requests
+import os
+
+# def __init__(self, link_list, image_list, price_list, city_list):
+#    self.linkList = link_list
+#    self.imageList = image_list
+#    self.priceList = price_list
+#    self.cityList = city_list
+linkList = ["https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html"]
+cityList = ["Asheville","Asheville","Charlotte","Weaverville","Charlotte","Asheville","Wilmington", "Raleigh", "Asheville","Waynesville"]
+#imageList = ["https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html"]
+priceList = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+
+
+
+
+
+
 root = Tk()
-
-frame = Frame(root)
-
-labelText = StringVar()
-
-label = Label(frame, textvariable = labelText)
-button = Button(frame, text = "Click me")
-
-labelText.set("I am a label")
-
-label.pack()
-button.pack()
-frame.pack()
+path = "./TestImages/01.jpg"
+img = Image.open(path)
+print(type(img))
+tImg = ImageTk.PhotoImage(img) #converts to a TK image which can be used in any Tk widget
+#canvas = Canvas(root, width = 300, height = 300)
+#canvas.pack()
+#canvas.create_image(20, 20, anchor=NW, image=tImg)
 
 root.mainloop()
