0db9f8cee9e87be1b2ac068c1c6ccb82369d5c51
Claymazing <38538839+Claymazing@users.noreply.github.com>
2019-07-17 14:22:14 -0400

    Added code to create and close a dictionary (Currently commented out) and some image handling for Canvas widgets in tkinter

--- craigsGUI.py
+++ craigsGUI.py
@@ -1,32 +1,39 @@
 from tkinter import *
 from PIL import ImageTk, Image
-from io import BytesIO
-import requests
+import urllib.request
+import shutil
 import os
-
-# def __init__(self, link_list, image_list, price_list, city_list):
+#def __init__(self, link_list, image_link_list, price_list, city_list):
 #    self.linkList = link_list
 #    self.imageList = image_list
 #    self.priceList = price_list
 #    self.cityList = city_list
+
+image_link_list = ['https://images.craigslist.org/00x0x_78IA8DYuBAu_600x450.jpg']
 linkList = ["https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html"]
 cityList = ["Asheville","Asheville","Charlotte","Weaverville","Charlotte","Asheville","Wilmington", "Raleigh", "Asheville","Waynesville"]
-#imageList = ["https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html","https://asheville.craigslist.org/grd/d/weaverville-alpacas/6930923148.html"]
 priceList = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+image_list = []
 
+#os.mkdir('./testImages') #Create the directory where images will be stored
+imgNum = 0
+pathDirs = []
+for i in image_link_list:
+    urllib.request.urlretrieve(i, "./testimages/image" + str(imgNum) + ".jpg") #Save images to the newly created directory
+    imgNum += 1
+    pathDirs.append("./testImages/image" + str(imgNum) + ".jpg")
 
-
-
+#shutil.rmtree('./testImages') #Deletes the image directory and its contents
 
 
 
 root = Tk()
-path = "./TestImages/01.jpg"
-img = Image.open(path)
-print(type(img))
-tImg = ImageTk.PhotoImage(img) #converts to a TK image which can be used in any Tk widget
-#canvas = Canvas(root, width = 300, height = 300)
-#canvas.pack()
-#canvas.create_image(20, 20, anchor=NW, image=tImg)
+for i in pathDirs:
+    img = Image.open(i)
+    tImg = ImageTk.PhotoImage(img) #converts to a TK image which can be used in any Tk widget
+    #Need to make a dict which associates strings with canvas items so that I can add the images to the canvas items in a later for loop? (I think)
+canvas = Canvas(root, width = 300, height = 300)
+canvas.pack()
+canvas.create_image(20, 20, anchor=NW, image=tImg)
 
-root.mainloop()
+#root.mainloop()
